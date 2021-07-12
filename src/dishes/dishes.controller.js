5725177e50adaa9085972aaaea4ad733d0484622
Nick Terns <ternsna11@gmail.com>
2021-07-12 10:24:57 -0500

    first commit

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+
+//middleware
+
+
+const list (req, res, next) => {
+res.json({ data: dishes })
+}
+
+const
+
+module.exports = {
+    list,
+
+}
